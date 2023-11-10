62d7f1fafe068762d04024b2934b6d1d3a74b652
Anderson Casimiro <97445047+cassgkn@users.noreply.github.com>
2023-11-10 10:45:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+
+    fetch('https://api.github.com/users/cassgkn')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(json) {
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url;
+            followingElement.innerText = json.following;
+            reposElement.innerText = json.public_repos;
+            linkElement.href = json.html_url;
+            
+            if(json.follwers == undefined) {
+                followersElement.innerText = '0'
+            } else {
+                followersElement.innerText = json.follwers;
+            }
+        })
+})
